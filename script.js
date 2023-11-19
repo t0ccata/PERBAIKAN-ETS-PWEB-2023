5b9a6d1a9e9bb09bb4eed582813043b8db66bc0b
Abdurrahman Faiz <m.abdurrahman.faiz24@gmail.com>
2023-11-19 21:00:25 +0700

    ubah script.js

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-$document.ready(function(){
+$(document).ready(function () {
     $.ajax({
         url: 'https://api-berita-indonesia.vercel.app/antara/politik/',
         method: 'GET',
-        success: function(response){
+        success: function (response) {
             if (response.success) {
-                displayData(response.data.posts);
+                displayData(response.data);
             }
         },
-        error: function(error){
+        error: function (error) {
             console.error("Error fetching data : ", error);
         }
     });
 
-    function displayData(posts){
+    function displayData(response) {
         let output = '';
 
-        $.each(posts, function(key, post){
+        $.each(response.posts, function (key, post) {
             output += `
             <div class="col-md-4 mb-3 d-flex">
-                <div class="card flex-fill">
-                    <img src="${post.thumbnail}" alt="" class="card-img-top">
-                </div>
+            <div class="card" style="width: 18rem;">
+            <img src="${post.thumbnail}" class="card-img-top">
                 <div class="card-body shadow d-flex flex-column">
                     <h5 class="card-title">${post.title}</h5>
                     <p class="card-text flex-grow-1 text-justify">${post.description}</p>
                     <a href="${post.link}" class="btn btn-danger mt-auto">Lihat Selengkapnya</a>
                 </div>
+            </div>
             </div>
             `;
         });
